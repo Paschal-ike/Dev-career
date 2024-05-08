6d35d3597fe3d8582f7c16ca9e1acf6bf3da87f8
paschal-ike <paschalpaul268@gmail.com>
2024-05-08 19:58:52 +0100

    js file

---
+++ Assignment B/assets/script.js
@@ -0,0 +1,12 @@
+// Smooth scrolling for navigation links
+$(document).ready(function() {
+    $('a[href^="#"]').on('click', function(event) {
+      var target = $(this.getAttribute('href'));
+      if (target.length) {
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+      }
+    });
+  });
